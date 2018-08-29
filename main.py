3079e6d9c4f0d9ed1a961f44ac060fa773c1db25
Adam Peterson <apeterson2008@gmail.com>
2018-08-29 16:04:00 -0600

    Added lottery numbers

--- main.py
+++ main.py
@@ -5,34 +5,40 @@ import argparse
 import re
 from datetime import date
 
-def importCsvData(filename):
+def importCsvData(filename, glcode):
     sales = {}
     jrnls = []
-    with open(filename, 'rb') as f:
-        for line in f:
-            l = sanitizeDescription(line)
-            line = l.split(',')
-            if line[0][:3] == '101':
-                dept_no = line[0][-2:]
-                batch_no = line[12][-3:]
-                idate = line[3]
-                amt = round(float(line[7]), 2)
-                entry_type = line[5]
-                # print(batch_no, idate, amt, entry_type)
-                if entry_type == 'SD':
-                    jrnls.append(line)
-                else:
-                    if dept_no not in sales:
-                        sales[dept_no] = {batch_no: [idate, amt]}
-                    elif batch_no not in sales[dept_no]:
-                        sales[dept_no][batch_no] = [idate, amt]
+    try:
+        with open(filename, 'rb') as f:
+            for line in f:
+                l = sanitizeDescription(line)
+                line = l.split(',')
+                # print(line[0][:4], glcode)
+                if line[0][:4] == glcode:
+                    dept_no = line[0][-2:]
+                    batch_no = line[12][-3:]
+                    idate = line[3]
+                    amt = round(float(line[7]), 2)
+                    entry_type = line[5]
+                    # print(batch_no, idate, amt, entry_type)
+                    if entry_type == 'SD':
+                        jrnls.append(line)
                     else:
-                        sales[dept_no][batch_no][1] += amt
-    return [sales, jrnls]
+                        if dept_no not in sales:
+                            sales[dept_no] = {batch_no: [idate, amt]}
+                        elif batch_no not in sales[dept_no]:
+                            sales[dept_no][batch_no] = [idate, amt]
+                        else:
+                            sales[dept_no][batch_no][1] += amt
+        return [sales, jrnls]
+    except Exception as e:
+        print('Exception: {}'.format(str(e)))
 
-def printReport(data, report_name):
-    sales = data[0]
-    jrnls = data[1]
+def printReport(sales_data, lotto_data, report_name):
+    sales = sales_data[0]
+    jrnls = sales_data[1]
+    lotto_sales = lotto_data[0]
+    lotto_jrnl = lotto_data[1]
     # report_date = date.today()
     # date = report_date.strftime("%A %d. %B %Y")
     with open(report_name, 'w') as r:
@@ -49,17 +55,29 @@ def printReport(data, report_name):
         r.write('{}\r\n'.format('='*80))
         r.write('{:80,.2f}\r\n\r\n'.format(jrnl_total))
         for d in sorted(sales):
-            dept_total = 0
+            dept_sales_total = 0
+            dept_lotto_total = 0
+            dept_difference = 0
             r.write('Dept: {}\r\n'.format(d))
             r.write('{}\r\n'.format('='*80))
-            r.write('{:10}{:16}{:>54}\r\n'.format('Batch No.', 'Date', 'Amount'))
+            r.write('{:10}{:16}{:>30}{:>12}{:>12}\r\n'.format('Batch No.', 'Date', 'Amount', 'Lotto', 'Difference'))
             # print(d)
             for s in sorted(sales[d]):
-                r.write('{:10}{:16}{:54,.2f}\r\n'.format(s, sales[d][s][0], sales[d][s][1]))
-                dept_total += sales[d][s][1]
-            sales_total += dept_total
+                batch_no = s
+                idate = sales[d][s][0]
+                sales_amt = sales[d][s][1]
+                if d in lotto_sales and batch_no in lotto_sales[d]:
+                    lotto_amt = lotto_sales[d][s][1]
+                else:
+                    lotto_amt = 0
+                difference = sales_amt + lotto_amt
+                r.write('{:10}{:16}{:30,.2f}{:12,.2f}{:12,.2f}\r\n'.format(batch_no, idate, sales_amt, lotto_amt, difference))
+                dept_sales_total += sales_amt
+                dept_lotto_total += lotto_amt
+                dept_difference += difference
+            sales_total += dept_sales_total
             r.write('{}\r\n'.format('='*80))
-            r.write('{:80,.2f}\r\n\r\n'.format(dept_total))
+            r.write('{:56,.2f}{:12,.2f}{:12,.2f}\r\n\r\n'.format(dept_sales_total, dept_lotto_total, dept_difference))
         r.write('{}\r\n'.format('='*80))
         r.write('{:20}{:60,.2f}\r\n'.format('Sales Total', sales_total))
         r.write('{:20}{:60,.2f}\r\n'.format('Journal Total', jrnl_total))
@@ -77,5 +95,6 @@ if __name__ == "__main__":
     parser.add_argument('filename', help="Which file would you like to open")
     args = parser.parse_args()
     print(args.filename)
-    d = importCsvData(args.filename)
-    printReport(d, "101_Report.txt")
+    data1010 = importCsvData(args.filename, '1010')
+    data4608 = importCsvData('4608.csv', '4608')
+    printReport(data1010, data4608, "101_Report.txt")
